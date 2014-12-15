296a935fb676952ee91ba3316295c06ece3de81b
Ron Daryl Magno <ron.zohan@yahoo.com>
2014-12-15 21:10:30 +0800

    Added checks if score is None or not

--- scorer_app/scorer_app.py
+++ scorer_app/scorer_app.py
@@ -3,12 +3,15 @@ Controller file
 """
 
 from flask import Flask, render_template, request
+from _ast import IsNot
+from compute_total_score import ComputeTotalScore
 
-PLAYERS_TEAM1 = {'Eden Hazard': '1', 'a': '2', 'b': '3', 'c': '4', 'd': '5'}
+
+PLAYERS_TEAM1 = {'Eden Hazard': '1', 'Jackson': '2', 'March': '3', 'Apple': '4', 'Lincoln': '5'}
 PLAYERS_TEAM1_SCORE = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0}
 TEAM1 = {'players': PLAYERS_TEAM1, 'name': 'Chelsea', 'scores': PLAYERS_TEAM1_SCORE}
 
-PLAYERS_TEAM2 = {'e': '1', 'f': '2', 'g': '3', 'h': '4', 'i': '5'}
+PLAYERS_TEAM2 = {'Costa': '1', 'Terry': '2', 'Cahill': '3', 'Drogba': '4', 'Cech': '5'}
 PLAYERS_TEAM2_SCORE = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0}
 TEAM2 = {'players': PLAYERS_TEAM2, 'name': 'Liverpool', 'scores': PLAYERS_TEAM2_SCORE}
 
@@ -23,19 +26,33 @@ def hello_world():
     point_score = request.args.get('point_score')
     team = request.args.get('team')
 
-    try:
-        player = update_points(player_name, point_score, team)
-        return render_template('index.html', player_name=player_name, 
-                               point_score=TEAMS[team]['scores'][player])
-    except:
-        return render_template('index.html', player_name=None, 
-                               point_score=None)
+    player = update_points(player_name, point_score, team)
+    print player
+
+    if player is None:
+        return render_template('index.html', error_message='Player not found or score input error.')
+    else:
+        compute = ComputeTotalScore(TEAMS)
+        total = compute.compute_total()
+
+        total1 = total[0]
+        total2 = total[1]
+        return render_template('index.html', player_name=player_name,
+                               point_score=TEAMS[team]['scores'][player],
+                               team=team, total1=total1, total2=total2)
 
 
 def update_points(player_name, point_score, team):
+
+    if point_score is not None:
+        if int(point_score) < 1:
+            return None
+        elif int(point_score) > 3:
+            return None
+
     try:
         player = TEAMS[team]['players'][player_name]
-        TEAMS[team]['scores'][player] += point_score
+        TEAMS[team]['scores'][player] += int(point_score)
     except:
         player = None
 
@@ -44,4 +61,4 @@ def update_points(player_name, point_score, team):
 
 if __name__ == '__main__':
     update_points('Eden Hazard', 2, 'Chelsea')
-    #APP.run(debug=True)
+    APP.run(debug=True)
