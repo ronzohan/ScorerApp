2e6837284933c1cce6c416e1730d018c2866bf5e
Ron Daryl Magno <ron.zohan@yahoo.com>
2014-12-15 21:11:00 +0800

    Added input score test

--- test/unit/input_score_test.py
+++ test/unit/input_score_test.py
@@ -7,3 +7,11 @@ class TestInputScore(unittest.TestCase):
     def test_if_input_values_are_none(self):
         player = update_points(None, None, None)
         assert_equal(player, None)
+
+    def test_if_the_scores_are_updated(self):
+        player = update_points('Eden Hazard', '2', 'Chelsea')
+        assert_equal(player, str(1))
+
+    def test_if_score_input_is_less_than_0_or_more_than_4(self):
+        player = update_points('Eden Hazard', '4', 'Chelsea')
+        assert_equal(player, None)
