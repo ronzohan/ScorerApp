11605e5a2e1d7ab867737f35befbe980b438e6b6
Ron Daryl Magno <ron.zohan@yahoo.com>
2014-12-15 22:46:27 +0800

    Finished test for the scorer app

--- test/bdd/features/compute_total_score.feature
+++ test/bdd/features/compute_total_score.feature
@@ -1,11 +1,9 @@
 Feature: Compute all the scores from all the teams
 		 As a scorer, I want to compute all of the scores of the team
-		 
+		
 		 @compute
 		 Scenario: Compute all the scores
 		 Given all the teams are set
 		 Then I see scores
-		 |Team     |Score|
-		 |Chelsea  |0    |
-		 |Liverpool|0    |
-	
+		 |team1_score|team2_score|
+		 |2          |0          |
--- test/bdd/steps/compute_total_score_step.py
+++ test/bdd/steps/compute_total_score_step.py
@@ -1,7 +1,8 @@
 from lettuce import step, world
 from scorer_app.scorer_app import TEAMS, APP
 from webtest import TestApp
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_in
+from scorer_app.compute_total_score import ComputeTotalScore
 
 
 @step(u'Given all the teams are set')
@@ -13,4 +14,22 @@ def given_all_the_teams_are_set(step):
 
 @step(u'Then I see scores')
 def then_i_see_scores(step):
-    assert False, 'This step must be implemented'
+    world.player = 'Jackson'
+    world.point = 2
+    world.team = 'Chelsea'
+    form = world.response.forms['tally-form']
+    form['player_name'] = world.player
+    form['point_score'] = world.point
+    form['team'] = world.team
+    world.form_response = form.submit()
+
+    compute = ComputeTotalScore(TEAMS)
+    total = compute.compute_total()
+    player = world.TEAMS[world.team]['players'][world.player]
+
+    for row in step.hashes:
+        team1_score = row['team1_score']
+        team2_score = row['team2_score']
+
+    assert_equal(team1_score, str(total[0]))
+    assert_equal(team2_score, str(total[1]))
